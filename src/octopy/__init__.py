4a441d2abf159a3e2b3cac7b06b04412c7b6fb69
Sam P (Spury) <s.pur@outlook.com>
2026-02-28 16:28:18 +0000

    Add OctopusAPIError and OctopusAuthError to module exports

--- src/octopy/__init__.py
+++ src/octopy/__init__.py
@@ -1,7 +1,10 @@
 """Octopy - An async Python client for the Octopus Energy REST API."""
 
 from octopy.client import Octopy
+from octopy.exceptions import OctopusAPIError, OctopusAuthError
 
 __all__ = [
-    "Octopy"
+    "Octopy",
+    "OctopusAPIError",
+    "OctopusAuthError",
 ]
