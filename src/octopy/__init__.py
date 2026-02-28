92da61e6a1aeb87eecd002011dd1e8e38d9f4666
Sam P (Spury) <s.pur@outlook.com>
2026-02-28 17:23:09 +0000

    Add model imports to module exports

--- src/octopy/__init__.py
+++ src/octopy/__init__.py
@@ -2,9 +2,26 @@
 
 from octopy.client import Octopy
 from octopy.exceptions import OctopusAPIError, OctopusAuthError
+from octopy.models import (
+    Account,
+    Agreement,
+    ElectricityMeterPoint,
+    GasMeterPoint,
+    Meter,
+    Property,
+    Register,
+)
+
 
 __all__ = [
     "Octopy",
     "OctopusAPIError",
     "OctopusAuthError",
+    "Account",
+    "Agreement",
+    "ElectricityMeterPoint",
+    "GasMeterPoint",
+    "Meter",
+    "Property",
+    "Register",
 ]
